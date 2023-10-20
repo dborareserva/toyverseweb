d77d967f5cbb780d2b2034ebfe1dba732ffbf352
dborareserva <148562234+dborareserva@users.noreply.github.com>
2023-10-20 11:24:42 -0300

    arquivos site

---
+++ toyverseweb/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Toyverse</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+</head>
+<body>
+
+
+  <!-- 
+  <?php
+include("php/conexao.php");
+include("php/banco-cliente.php");
+
+?>
+ -->
+  <header>
+    <div class="header-container">
+      <img src="img/logo.png" alt="logo" style="width: 100px; height: auto; margin-right: 800px;">
+      <nav>
+        <ul>
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Produtos</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  
+  <section class="main-section">
+    <section class="espaço">
+    <h2 class="animate__animated animate__bounceIn">Esse é o Toyverse!</h2>
+  </section>
+  <section class="sobre">
+    <p class="animate__animated animate__bounceIn">
+      
+Bem-vindo ao mundo da inovação e diversão! Aqui, na nossa startup de brinquedos inteligentes, estamos apaixonados por criar experiências únicas que combinam a magia dos brinquedos com a inteligência da tecnologia. Nossa missão é trazer sorrisos aos rostos das crianças e estimular suas mentes curiosas através de produtos que encantam, ensinam e desafiam.</p>
+    </section>
+
+  <h2 class="animate__animated animate__bounceIn">Nossos produtos!<h2 class="idprodutos"></h2>
+</h2>
+
+    <div class="products">
+      <div class="product">
+        <img src="product1.jpg" alt="Produto 1">
+        <h3>Produto 1</h3>
+        <p>R$ 99,99</p>
+       
+        <a href="compra1.html"><button class="buy-button" data-product-id="1">Comprar</button></a>
+
+        
+         
+      </div>
+      <div class="product">
+        <img src="product2.jpg" alt="Produto 2">
+        <h3>Produto 2</h3>
+        <p>R$ 149,99</p>
+        <button class="buy-button" data-product-id="2">Comprar</button>
+      </div>
+      <div class="product">
+        <img src="product3.jpg" alt="Produto 2">
+        <h3>Produto 3</h3>
+        <p>R$ 149,99</p>
+        <button class="buy-button" data-product-id="3">Comprar</button>
+      </div>
+      
+    </div>
+  </section>
+    
+
+    
+    <section class="contato">
+      <h2 class="animate__animated animate__bounceIn">Nosso contato</h2>
+      <p>toyverse.com.br</p>
+    </section>
+  </main>
+  
+  <footer>
+      
+    <p class="copyright">&copy; 2023 Toyverse. Todos os direitos reservados.</p>
+   
+  <img src="img/logo.png" class="footer-logo" alt="Logo Toyverse">
+  </footer>
+  <script src="js/script.js"></script>
+</body>
+</html>
